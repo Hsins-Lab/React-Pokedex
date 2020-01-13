c6f9c79b01e8aef04706d714b42515e178bc07b3
Hsins <hsinspeng@gmail.com>
2020-01-13 20:39:08 +0800

    Updates

---
+++ precache-manifest.d6cb89f23d16e002b9fe725c70fcbd07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3048341a12cdc4f7eed52ada770321a",
+    "url": "/udemy_React-Pokedex/index.html"
+  },
+  {
+    "revision": "6af01ca8948ad077dfae",
+    "url": "/udemy_React-Pokedex/static/css/main.cbbce599.chunk.css"
+  },
+  {
+    "revision": "5f1e625857bfc5a34d2b",
+    "url": "/udemy_React-Pokedex/static/js/2.b2248400.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/udemy_React-Pokedex/static/js/2.b2248400.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6af01ca8948ad077dfae",
+    "url": "/udemy_React-Pokedex/static/js/main.4c37c17c.chunk.js"
+  },
+  {
+    "revision": "b6f1c81a7c99642ebeb5",
+    "url": "/udemy_React-Pokedex/static/js/runtime-main.745e80c3.js"
+  }
+]);
